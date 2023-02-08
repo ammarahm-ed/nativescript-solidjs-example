248514e6a7b72e081a01d74f85395b78a93963d2
ammarahm-ed <ammarahmed6506@gmail.com>
2023-02-08 07:56:32 +0500

    listview

--- app/home.tsx
+++ app/home.tsx
@@ -1,70 +1,54 @@
-import {
-  children,
-  Component,
-  createSignal,
-  getOwner,
-  Owner,
-  runWithOwner,
-} from "solid-js";
+import { Component, createSignal } from "solid-js";
+import { List } from "./list";
 
-const Item: Component<{ item?: () => any; index?: () => number }> = (props) => {
-  return (
-    <label text={props.index?.() + ""}/>
+const Item: Component<{
+  item?: () => any;
+  index?: () => number;
+  type: () => string;
+}> = (props) => {
+  return props.type() === "even" ? (
+    <flexboxlayout style={{ height: 300, backgroundColor: "white" }}>
+      <label text={props.index?.() + " " + props.type()} />
+    </flexboxlayout>
+  ) : (
+    <flexboxlayout style={{ height: 300, backgroundColor: "red" }}>
+      <label text={props.index?.() + " " + props.type()} />
+    </flexboxlayout>
   );
 };
 
 export const Home = () => {
-  const owner = getOwner();
+  const [items, setItems] = createSignal([0]);
   return (
     <>
       <actionbar />
-      <flexboxlayout style="background-color:red">
-        <listview
-          items={{
-            length: 200,
-            getItem(index) {
-              return {
-                index: index,
-              };
-            },
+      <flexboxlayout flexDirection="column">
+        <button
+          text="Add item"
+          on:tap={() => {
+            setItems((prev) => {
+              return [...prev, prev.length];
+            });
           }}
-          itemTemplateSelector={(item, index) => {
-            return "even";
+          style={{
+            height:50,
+            minHeight: 50
           }}
-        >
-          <arrayprop key="itemTemplates">
-            <itemtemplate
-              key="even"
-              on:itemLoading={(event) => {
-                const { view, item, index } = event;
-                console.log("itemLoading", view, index);
-                const context = (view as any).___solidJSContext();
-                if (context) {
-                  runWithOwner(context.owner, () => {
-                    context.setItem(item);
-                    context.setIndex(index);
-                   
-                  });
-                }
-              }}
-              on:createView={(event) => {
-                runWithOwner(owner as Owner, () => {
-                  const [item, setItem] = createSignal();
-                  const [index, setIndex] = createSignal<number>(0);
-                  const element = children(() => (
-                    <Item item={item} index={index} />
-                  ));
-                  event.view = element();
-                  (event.view as any).___solidJSContext = () => ({
-                    setItem,
-                    setIndex,
-                    owner,
-                  });
-                });
-              }}
-            />
-          </arrayprop>
-        </listview>
+        />
+        <contentview style={{
+          flexShrink :1
+        }}>
+          <List
+            itemTypes={["even", "odd"]}
+            items={items()}
+            renderItem={({ item, index, type }) => (
+              <Item item={item} index={index} type={type} />
+            )}
+            onItemType={(item, index) => {
+              return index % 2 === 0 ? "even" : "odd";
+            }}
+          />
+        </contentview>
       </flexboxlayout>
     </>
   );
